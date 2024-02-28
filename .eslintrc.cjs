3f261823f7982c7af8d10c0e553d6fbb983228ce
AndreyDerunow <derunov.andreyy@gmail.com>
2024-02-28 12:57:40 +0400

    added configs for prettier, ts, eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,53 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    root: true,
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    },
+    env: { browser: true, es2021: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended'
     ],
-  },
-}
+    ignorePatterns: ['dist', '.eslintrc.cjs', '**.css'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh', 'prettier'],
+    rules: {
+        'no-console': ['error', { allow: ['warn', 'error'] }],
+        quotes: ['error', 'single'],
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true }
+        ],
+        'prettier/prettier': [
+            'warn',
+            {
+                endOfLine: 'auto'
+            }
+        ],
+        '@typescript-eslint/consistent-type-imports': [
+            'error',
+            {
+                prefer: 'type-imports'
+            }
+        ],
+        'sort-imports': [
+            'error',
+            {
+                ignoreCase: true,
+                ignoreDeclarationSort: false,
+                ignoreMemberSort: false,
+                memberSyntaxSortOrder: ['single', 'multiple', 'all', 'none'],
+                allowSeparatedGroups: false
+            }
+        ],
+        'import/prefer-default-export': 'off',
+        'react/jsx-no-useless-fragment': ['error', { allowExpressions: true }],
+        curly: 'error',
+        'no-unused-expressions': 'error'
+    }
+};
