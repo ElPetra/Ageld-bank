9e70fbbbad5de724e605cb790dc4c6f3ae2fe130
Никита Асауляк <nikitaasaulak@MacBook-Pro-Nikita.local>
2024-03-05 20:36:04 +0300

    feat: agel-947 добавил страницу регистрации и часть компонентов в неё

--- src/app/router/app-router.tsx
+++ src/app/router/app-router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import { AuthPage } from 'src/pages/auth';
 import { Layout } from 'src/pages/layout';
 import { MainPage } from 'src/pages/main';
+import { RegisterPage } from 'src/pages/register/ui';
 
 export const AppRouter = () => {
     return (
@@ -9,6 +10,7 @@ export const AppRouter = () => {
             <Route path='/' element={<Layout />}>
                 <Route index path='/' element={<MainPage />} />
                 <Route path='/auth' element={<AuthPage />} />
+                <Route path='/register' element={<RegisterPage />} />
             </Route>
         </Routes>
     );
---
+++ src/pages/register/ui/index.tsx
@@ -0,0 +1,24 @@
+import { Text } from 'src/shared/ui';
+import { Greeting } from 'src/widgets/greeting';
+import './styles.scss';
+
+export const RegisterPage = () => {
+    return (
+        <div className='register'>
+            <div className='greeting'>
+                <Greeting />
+                <div className='greeting__warning-text'>
+                    <Text size='l' weight='regular'>
+                        Доступ в личный кабинет возможен с телефонного номера,
+                        указанного при открытии счёта в нашем банке.
+                        <br />
+                        <br />
+                        Для создания кабинета пользователя нажмите кнопку
+                        РЕГИСТРАЦИЯ.
+                    </Text>
+                </div>
+            </div>
+            <div className='action-box'>компонент для взаимодействия</div>
+        </div>
+    );
+};
