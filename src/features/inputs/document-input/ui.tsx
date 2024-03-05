ddb3954d2d671286ec31fd543753a5abdb3fa5cc
Olesya Dorofeeva <lesjok@inbox.ru>
2024-03-05 23:37:08 +0300

    feat: agel-947 добавлен input для ввода номера документа

---
+++ src/features/inputs/document-input/ui.tsx
@@ -0,0 +1,50 @@
+import { type InputHTMLAttributes, useState } from 'react';
+
+import { Icon, Input } from 'src/shared/ui';
+
+import type { ChangeEvent } from 'react';
+import type { FieldValues, UseFormRegister } from 'react-hook-form';
+
+interface Props extends Omit<InputHTMLAttributes<HTMLInputElement>, 'size'> {
+    width?: 'auto' | 'max';
+    error?: boolean;
+    clear: () => void;
+    label: string;
+    register: UseFormRegister<FieldValues>;
+}
+
+export const DocumentInput = ({ clear, error, ...props }: Props) => {
+    const [value, setValue] = useState<string>('');
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+        let inputValue = e.target.value;
+        inputValue = inputValue.replace(/\D/gm, '');
+        if (inputValue.length <= 4) {
+            e.target.value = inputValue.replace(/(\d{2})/, '$1 ');
+        } else if (inputValue.length <= 20) {
+            e.target.value = inputValue.replace(
+                /(\d{2})(\d{2})(\d{1,6})/,
+                '$1 $2 $3'
+            );
+        }
+        setValue(e.target.value);
+    };
+
+    return (
+        <Input
+            type='tel'
+            maxLength={20}
+            minLength={6}
+            placeholder='Серия и номер паспорта/ВНЖ'
+            size='large'
+            value={value}
+            onChange={handleChange}
+            error={error ? 'Документ должен содержать от 6 до 20 символов' : ''}
+            {...props}
+        >
+            <button type='button' onClick={clear}>
+                <Icon icon='clear' />
+            </button>
+        </Input>
+    );
+};
--- src/pages/main/ui/index.tsx
+++ src/pages/main/ui/index.tsx
@@ -2,6 +2,7 @@ import { useForm } from 'react-hook-form';
 
 import { Button, Form, Link } from 'src/shared/ui';
 import { PasswordInput, PhoneInput, CodeInput } from 'src/features/inputs';
+import { DocumentInput } from 'src/features/inputs/document-input';
 
 export const MainPage = () => {
     const {
@@ -25,6 +26,12 @@ export const MainPage = () => {
                     register={register}
                     label='password'
                     error={!!errors?.password}
+                />
+                <DocumentInput
+                    clear={() => setValue('document', '')}
+                    label={'document'}
+                    register={register}
+                    error={!!errors?.document}
                 />
                 <Button variant='secondary' size='large' type='submit'>
                     Далее
