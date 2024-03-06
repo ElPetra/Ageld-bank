2aa40044e0410bf85ed8514a962a316eb6163f19
Olesya Dorofeeva <lesjok@inbox.ru>
2024-03-06 17:16:33 +0300

    feat: agel-946 добавлены шаги в форму

--- src/features/multi-step-form/ui.tsx
+++ src/features/multi-step-form/ui.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
-import { PhoneInput } from 'src/features/inputs';
+import { CodeInput, PhoneInput } from 'src/features/inputs';
 import { Button, Form } from 'src/shared/ui';
 
 import { BackButton } from './go-back';
@@ -12,7 +12,6 @@ import './styles.scss';
 interface Props {
     variant?: 'login' | 'registration' | 'none';
 }
-
 export const MultiStepForm = ({ variant = 'none' }: Props) => {
     const {
         register,
@@ -20,21 +19,31 @@ export const MultiStepForm = ({ variant = 'none' }: Props) => {
         handleSubmit,
         formState: { errors }
     } = useForm({ mode: 'onBlur', reValidateMode: 'onChange' });
-    const [formStep, setFormStep] = useState<number>(2);
+    const [formStep, setFormStep] = useState<number>(0);
+
+    const onSubmit = (data: Props) => {
+        console.log(data);
+        setFormStep(curr => curr + 1);
+    };
 
     return (
         <div className='multi-step-form'>
-            {formStep > 1 && (
+            {formStep > 0 && (
                 <BackButton onClick={() => setFormStep(curr => curr - 1)} />
             )}
             <FormCard title='Регистрация' variant={variant}>
-                <Form onSubmit={handleSubmit(data => console.log(data))}>
-                    <PhoneInput
-                        clear={() => setValue('phone', '')}
-                        label={'phone'}
-                        register={register}
-                        error={!!errors?.phone}
-                    />
+                <Form onSubmit={handleSubmit(onSubmit)}>
+                    {formStep === 0 && (
+                        <PhoneInput
+                            clear={() => setValue('phone', '')}
+                            label={'phone'}
+                            register={register}
+                            error={!!errors?.phone}
+                        />
+                    )}
+                    {formStep === 1 && (
+                        <CodeInput label='sms' register={register} error={''} />
+                    )}
                     <Button variant='secondary' size='large' type='submit'>
                         Далее
                     </Button>
