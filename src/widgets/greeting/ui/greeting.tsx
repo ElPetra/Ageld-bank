a9cfd31294ef314b2b9c62cd92ce60d467a54ef1
Никита Асауляк <nikitaasaulak@MacBook-Pro-Nikita.local>
2024-03-13 09:56:22 +0300

    fix: agel-946 обновил выравнивание текста

--- src/widgets/greeting/ui/greeting.tsx
+++ src/widgets/greeting/ui/greeting.tsx
@@ -5,7 +5,7 @@ export const Greeting = () => {
     return (
         <div className='greeting'>
             <Icon icon='logo-title' width={129} height={108} />
-            <Text tag='h1' size='xl' weight='bold'>
+            <Text tag='h1' size='xl' weight='bold' align='center'>
                 Приветствуем <br /> в интернет-банкинге!
             </Text>
         </div>
