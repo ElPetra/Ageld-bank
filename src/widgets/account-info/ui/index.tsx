0733d73cc3350c177abbf57eb7087efb66e2e5c5
AndreyDerunow <derunov.andreyy@gmail.com>
2024-03-29 02:58:42 +0400

    feat: AGEL-793 добавлена страница информации о счете по US 4.2

--- src/widgets/account-info/ui/index.tsx
+++ src/widgets/account-info/ui/index.tsx
@@ -1,11 +1,41 @@
-import { useParams } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-import { accounts } from 'src/widgets/accounts/model';
+import { Icon, Text } from 'src/shared/ui';
+
+import { Menu } from 'src/features/menu';
+
+import { AccountOperations } from 'src/widgets/account-operations';
+
+import { ACCOUNT_INFO, OPERATIONS_HISTORY } from '../model/constants';
+
+import { AccountInfoContent } from './infoContent/AccountInfoContent';
 
 import './styles.scss';
 
 export const AccountInfo = () => {
-    const { id } = useParams();
-    const currentAccount = accounts.find(el => el.id === id);
-    return <div>{currentAccount?.balance}</div>;
+    const { state } = useLocation();
+
+    return (
+        <>
+            <Link className='back_link' to={state.from}>
+                <Icon icon='arrow' />
+                <Text>Назад</Text>
+            </Link>
+            <Menu
+                variant={'secondary'}
+                elements={[
+                    {
+                        id: 1,
+                        name: ACCOUNT_INFO,
+                        component: <AccountInfoContent />
+                    },
+                    {
+                        id: 2,
+                        name: OPERATIONS_HISTORY,
+                        component: <AccountOperations />
+                    }
+                ]}
+            ></Menu>
+        </>
+    );
 };
--- src/widgets/accounts/lib/index.ts
+++ src/widgets/accounts/lib/index.ts
@@ -27,3 +27,8 @@ export const useAccountsFilter = (
 
     return [[currency, setCurrency], getSelectedAccounts];
 };
+
+export const checkAccountAvailable = (account: Account) =>
+    account.status !== 'closed' ||
+    (account.closed &&
+        +account.closed < +account.closed + 3 * 30 * 24 * 60 * 60 * 1000);
