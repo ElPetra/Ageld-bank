1465c3e0149fadfb47411e737e4d40c585bf4928
Никита Асауляк <nikitaasaulak@MacBook-Pro-Nikita.local>
2024-04-03 15:49:57 +0300

    fix: AGEL-950 почистил ненужный код

--- src/pages/main/ui/index.tsx
+++ src/pages/main/ui/index.tsx
@@ -1,15 +1,5 @@
 import { Greeting } from 'src/widgets/greeting';
-import { useCheckStatusMutation } from 'src/shared/api';
-import { useEffect } from 'react';
 
 export const MainPage = () => {
-    const [func] = useCheckStatusMutation();
-
-    useEffect(() => {
-        func('79234251422');
-    }, []);
-
-    //useGetInfoQuery({ Authorization: tokenData?.accessToken || '' });
-
     return <Greeting />;
 };
