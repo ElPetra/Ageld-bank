9bc6035063e9a96f6a0fb858113a95cf8d227efa
Никита Асауляк <nikitaasaulak@MacBook-Pro-Nikita.local>
2024-04-04 10:11:21 +0300

    fix: AGEL-950 переписал navigation в более удобный формат

--- src/widgets/header/ui/navigation/ui.tsx
+++ src/widgets/header/ui/navigation/ui.tsx
@@ -1,8 +1,7 @@
 import { Link } from 'src/shared/ui';
 import { useAuth } from 'src/shared/hooks/useAuth';
 import { Logout } from 'src/features/logout';
-
-import { links, privateLinks, publicLinks } from './config';
+import { RouteName } from 'src/shared/model';
 
 import './styles.scss';
 
@@ -11,29 +10,25 @@ export const Navigation = () => {
     return (
         <nav className='navigation'>
             <ul className='navigation-default'>
-                {links.map(el => (
-                    <Link key={el.text} to={el.href}>
-                        {el.text}
-                    </Link>
-                ))}
+                <Link to={RouteName.MAIN_PAGE}>Банкоматы и отеделения</Link>
+                <Link to={RouteName.MAIN_PAGE}>Курсы валют</Link>
+                <Link to={RouteName.MAIN_PAGE}>Контакты</Link>
+                <Link to={RouteName.MAIN_PAGE}>Список услуг</Link>
+                <Link to={RouteName.MAIN_PAGE}>Документы</Link>
+                <Link to={RouteName.MAIN_PAGE}>Вопросы и ответы</Link>
             </ul>
             <ul className='navigation-additional'>
                 {isAuth ? (
                     <>
-                        {privateLinks.map(el => (
-                            <Link key={el.text} to={el.href}>
-                                {el.text}
-                            </Link>
-                        ))}
+                        <Link to={RouteName.PERSONAL_PAGE}>Личные данные</Link>
                         <Logout />
                     </>
                 ) : (
                     <>
-                        {publicLinks.map(el => (
-                            <Link key={el.text} to={el.href}>
-                                {el.text}
-                            </Link>
-                        ))}
+                        <Link to={RouteName.LOGIN_PAGE}>Войти</Link>
+                        <Link to={RouteName.REGISTRATION_PAGE}>
+                            Регистрация
+                        </Link>
                     </>
                 )}
             </ul>
