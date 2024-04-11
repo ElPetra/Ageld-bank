2199054775de688d1bdad38bf23552c429927ea2
Anastasia <juliadeiker@yandex.ru>
2024-04-11 09:10:22 +0300

    feat: AGEL-1059 изменено Logout

--- src/features/logout/ui.tsx
+++ src/features/logout/ui.tsx
@@ -1,19 +1,7 @@
-import { useNavigate } from 'react-router-dom';
 import { useAppDispatch } from 'src/app/store/dispatch';
 import { removeUser } from 'src/app/store/slices/userSlice';
-import { RouteName } from 'src/shared/model';
 
 export const Logout = () => {
     const dispatch = useAppDispatch();
-    const navigate = useNavigate();
-    return (
-        <button
-            onClick={() => {
-                dispatch(removeUser());
-                navigate(RouteName.MAIN_PAGE);
-            }}
-        >
-            Выйти
-        </button>
-    );
+    return <button onClick={() => dispatch(removeUser())}>Выйти</button>;
 };
