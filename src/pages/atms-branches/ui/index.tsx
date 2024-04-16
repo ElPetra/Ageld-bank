c622b3268a66cf94abdc7871a08c4125bbdc661a
Anastasia <juliadeiker@yandex.ru>
2024-04-16 13:27:27 +0300

    feat: AGEL-1044 добавлено react-yandex-maps

--- src/pages/atms-branches/ui/index.tsx
+++ src/pages/atms-branches/ui/index.tsx
@@ -1,11 +1,24 @@
+import { Map, YMaps, SearchControl } from '@pbe/react-yandex-maps';
+
+import './styles.scss';
+
 export const ATMsBranchesPage = () => {
     return (
-        <>
-            <iframe
-                src='https://yandex.ru/map-widget/v1/?um=constructor%3Ab9ea031151bbff03126f3e3dff1db1283240c9f860622376b9664846d4cfa129&amp;source=constructor'
+        <YMaps>
+            <Map
+                defaultState={{
+                    center: [55.751574, 37.573856],
+                    zoom: 9,
+                    controls: []
+                }}
                 width='100%'
-                height='720'
-            ></iframe>
-        </>
+                height='720px'
+            >
+                <SearchControl
+                    className='search-control'
+                    options={{ float: 'right', size: 'large' }}
+                />
+            </Map>
+        </YMaps>
     );
 };
