b7b49d21ce9c3aaccd024f1b705b26dc86303056
Anastasia <juliadeiker@yandex.ru>
2024-04-17 10:36:40 +0300

    fix: AGEL-1044 исправлены конфликты

--- src/pages/personal/ui/index.tsx
+++ src/pages/personal/ui/index.tsx
@@ -6,7 +6,6 @@ import { MultiStepForm } from 'src/features/multi-step-form';
 import { CheckboxGroup } from 'src/widgets/notifications';
 
 import { PersonalData } from './personal-data';
-
 import { options } from '../model';
 
 export const PersonalPage = () => {
