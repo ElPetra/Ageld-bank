36d1269377248be700585690586217f3dd4f7c89
Artyom Rulinskii <a.rulinskiy@astondevs.ru>
2024-05-16 11:37:22 +0300

    fix: AGEL-1161 Фикс импортов

--- src/widgets/deposits/index.ts
+++ src/widgets/deposits/index.ts
@@ -1,3 +1,3 @@
-export { DepositProduct } from 'src/shared/model';
+export { DepositProducts } from './ui/deposit_products/ui'
 export { CustomerDeposits } from './ui/customer-deposits/ui';
 export { Deposits } from './ui'
--- src/widgets/deposits/ui/deposit_products/ui.tsx
+++ src/widgets/deposits/ui/deposit_products/ui.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export const DepositProducts = () => {
     return <div></div>;
 };
