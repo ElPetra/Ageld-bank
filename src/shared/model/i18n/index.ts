d3f28b4f95a54a27693d9cfab77fd7707e00878e
Anastasia <juliadeiker@yandex.ru>
2024-06-20 12:35:18 +0300

    fix: i18n

--- src/shared/model/i18n/index.ts
+++ src/shared/model/i18n/index.ts
@@ -3,6 +3,9 @@ import { initReactI18next } from 'react-i18next';
 import HttpBackend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+import enJSON from 'src/shared/model/i18n/locales/en/en.json';
+import ruJSON from 'src/shared/model/i18n/locales/ru/ru.json';
+
 i18n.use(HttpBackend)
     .use(LanguageDetector)
     .use(initReactI18next)
@@ -11,8 +14,9 @@ i18n.use(HttpBackend)
         interpolation: {
             escapeValue: false
         },
-        backend: {
-            loadPath: 'src/shared/model/i18n/locales/{{lng}}/{{lng}}.json'
+        resources: {
+            en: { translation: enJSON },
+            ru: { translation: ruJSON }
         }
     });
 
