b0473c79a31080dfac4b102a59a09a84c19cd22a
Anastasia <juliadeiker@yandex.ru>
2024-07-11 11:15:09 +0300

    refactor: AGEL-1711 удаление комментариев

--- src/pages/main/ui/index.tsx
+++ src/pages/main/ui/index.tsx
@@ -62,31 +62,3 @@ export const MainPage = () => {
         </Container>
     );
 };
-
-// const [value, setValue] = useState<string>('');
-// const { register, handleSubmit } = useForm<FieldValues>({
-//     mode: 'onSubmit',
-//     reValidateMode: 'onSubmit',
-//     defaultValues: { selector: '' }
-// });
-//
-// <Card>
-//     <form onSubmit={handleSubmit(data => console.log(data))}>
-//         <Select
-//             variant='secondary'
-//             label='Variant'
-//             options={[
-//                 { value: 'Variant 1', label: 'Variant 1' },
-//                 { value: 'Variant 2', label: 'Variant 2' },
-//                 { value: 'Variant 3', label: 'Variant 3' },
-//                 { value: 'Variant 4', label: 'Variant 4' },
-//                 { value: 'Variant 5', label: 'Variant 5' }
-//             ]}
-//             register={register}
-//             onChange={e => setValue(e.target.value)}
-//             value={value}
-//             field='selector'
-//         />
-//         <input type='submit' />
-//     </form>
-// </Card>
