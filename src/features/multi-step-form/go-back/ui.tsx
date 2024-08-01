c39b796459b0a75e5e89088d117742794a744d24
Anastasia <juliadeiker@yandex.ru>
2024-08-01 20:17:09 +0300

    feat: AGEL-1786 добавлено classnames в BackButton

--- src/features/multi-step-form/go-back/ui.tsx
+++ src/features/multi-step-form/go-back/ui.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import cn from 'classnames';
 
 import { Icon, Text } from 'src/shared/ui';
 
@@ -13,10 +14,13 @@ interface Props {
 export const BackButton = ({ onTop, onClick }: Props) => {
     const navigate = useNavigate();
     const { t } = useTranslation();
+    const backButtonClass = cn('back-button', {
+        'back-button__top': !onClick || onTop
+    });
 
     return (
         <button
-            className={`back-button ${(!onClick || onTop) && 'back-button__top'}`}
+            className={backButtonClass}
             onClick={() => (onClick ? onClick() : navigate(-1))}
         >
             <Icon icon='arrow-left-accent' />
